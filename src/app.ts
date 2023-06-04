6e9a8019e6d0821a2c75ba18bfe9dd54c4d0e49b
mdrayhanhowlader <rhowlader5314@gmail.com>
2023-06-04 22:10:45 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
-const port = 5000;
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 
 //parser
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
 
-export default app;
+export default app
