96f654711d3845323dfb6f036413f9f22aa4a12a
mdrayhanhowlader <rhowlader5314@gmail.com>
2023-07-17 00:20:24 +0600

    setup prettier huskey

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
-    console.log(`Database Successfully Connected.`);
+      console.log(`Example app listening on port ${config.port}`)
+    })
+    console.log(`Database Successfully Connected.`)
   } catch (err) {
-    console.log(`Failed to connect database`, err);
+    console.log(`Failed to connect database`, err)
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-main();
+main()
